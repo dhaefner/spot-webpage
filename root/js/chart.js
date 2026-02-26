d9e4c7abdbd93d46f3a65cc4279580484df68180
David Häfner <david.haefner2006@gmx.de>
2026-02-26 14:22:45 +0100

    Update chart.js

--- root/js/chart.js
+++ root/js/chart.js
@@ -1,14 +1,13 @@
 
 function parseDate(dateInput) {
     try {
-        const rawDate = (document.getElementById(dateInput).value || '').toString().trim();
         let apiDate = '';
-        if (/^\d{4}-\d{2}-\d{2}$/.test(rawDate)) {
-            apiDate = rawDate.replace(/-/g, '');
-        } else  if (/^\d{8}$/.test(rawDate)) {
-            apiDate = rawDate;
-        } else if (rawDate) {
-            const digs = rawDate.replace(/[^0-9]/g, '');
+        if (/^\d{4}-\d{2}-\d{2}$/.test(dateInput)) {
+            apiDate = dateInput.replace(/-/g, '');
+        } else  if (/^\d{8}$/.test(dateInput)) {
+            apiDate = dateInput;
+        } else if (dateInput) {
+            const digs = dateInput.replace(/[^0-9]/g, '');
             apiDate = (digs + '00000000').slice(0, 8);
         } else {
             apiDate = '20250101'
